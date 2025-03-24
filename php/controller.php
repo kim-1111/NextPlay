c332d974e89ff81ec2b66b915620e5c792a7792e
kim-1111 <em.huang.yixin@gmail.com>
2025-03-24 13:14:59 +0100

    Co-authored-by: Junxi-HM <Junxi-HM@users.noreply.github.com> Co-authored-by: Adam <askm6@users.noreply.github.com> Co-authored-by: Justin Suárez <zjsuarez@users.noreply.github.com>

--- php/controller.php
+++ php/controller.php
@@ -2,51 +2,82 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  $user = new UserController();
-  if (isset($_POST["login"])) {
-    $user ->login();
-  }
-  if (isset($_POST["logout"])) {
-    $user ->logout();
-  }
-  if (isset($_POST["register"])) {
-    $user ->register();
-  }
+    $user = new UserController();
+    if (isset($_POST["login"])) {
+        $user->login();
+    }
+    if (isset($_POST["logout"])) {
+        $user->logout();
+    }
+    if (isset($_POST["register"])) {
+        $user->register();
+    }
 }
 
-class UserController{
+class UserController
+{   
+    private $conn;
+    function __construct()
+    {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = " ";
+        $dbname = "nextplay";
+
+        // Create connection
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
 
     private $staticUser = [
         'username' => 'user',
         'password' => '1234'
-      ];
+    ];
 
-  public function login(){
+    public function login()
+    {
 
-    $username = $_POST['username'];
-    $passwd = $_POST['password'];
+        $username = $_POST['username'];
+        $passwd = $_POST['password'];
 
-    if ($username == $this->staticUser['username'] && $passwd == $this->staticUser['password']) {
-      $_SESSION['user'] = $username;
-      $_SESSION['success'] = "Inicio de sesión exitoso!";
-      echo $_SESSION['success'];
-      echo $_SESSION['user'];
+        $stmt = $this->conn->prepare(query: "SELECT nombre, contrasena FROM usuarios WHERE nombre = ? AND contrasena = ?");
+        $stmt->bind_param("ss", $username, $passwd);
+        $stmt->execute();
 
-    } else {
-      $_SESSION['error'] = "Credenciales inválidas";
-      echo $_SESSION['error'];
+        if($stmt->fetch()){
+            $_SESSION['logged'] = true;
+            $_SESSION['user'] = $username;
+
+            $this->conn->close();
+
+            header(header: "Location: ../HTML/xxxx");
+            exit();
+            
+        }else{
+            $_SESSION['logged'] = false;
+            
+
+        }
+        
+        
+
+
+        /* if ($username == $this->staticUser['username'] && $passwd == $this->staticUser['password']) {
+            $_SESSION['user'] = $username;
+            $_SESSION['logged'] = "Inicio de sesión exitoso!";
+            echo $_SESSION['logged'];
+            echo $_SESSION['user'];
+        } else {
+            $_SESSION['error'] = "Credenciales inválidas";
+            echo $_SESSION['error'];
+        }*/
     }
 
+    public function logout() {}
 
-    
-  }
-
-  public function logout(){
-    
-  }
-
-  public function register(){
-    
-  }
+    public function register() {}
 }
-?>
